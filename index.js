5df44e49686b2ef3fde559f3992f546d1931521d
Siarhei Saltanovich <siarhei_saltanovich@epam.com>
2021-08-07 13:09:50 +0300

    feat: implement base functionallity T1

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import myUtil from './src/util.js';
 
 const createChatButton = document.querySelector('.createChat');
 const chatNames = document.querySelector('.chatNames');
-const users = document.querySelector('.users');
+const usersBlock = document.querySelector('.users');
 const sendButton = document.querySelector('.send');
 const inputMessage = document.querySelector('.currMessage');
 const blockMessages = document.querySelector('.block-messages');
 
+const myInfo = {};
 const webSocketServer = new WebSocket('ws://localhost:8080');
 webSocketServer.onopen = () => {
     console.log('open');
@@ -16,6 +17,23 @@ webSocketServer.onmessage = (responce) => {
     const type = json.type;
     if (type === 'message') {
         myUtil.addMessage(json.name, json.message, blockMessages);
+    } else if (type === 'connect') {
+        myInfo.name = json.myName;
+        myInfo.ID = json.myID;
+        myInfo.currentChatID = json.chatID; 
+
+        json.users.forEach(user => myUtil.addPesonInCurrentChat(user, '', usersBlock));
+        
+    } else if (type === 'newUser') {
+        myUtil.addPesonInCurrentChat(json, '', usersBlock);
+    } else if (type === 'userLeft') {
+        while(usersBlock.children.length) {
+            usersBlock.children[0].remove();
+        }
+        for (const user of json.users) {
+            myUtil.addPesonInCurrentChat(user, '', usersBlock);
+        }
+        myUtil.userLeftMessage('', json.nameOfUser, blockMessages);
     }
 }
 sendButton.addEventListener('click', () => {
@@ -23,8 +41,11 @@ sendButton.addEventListener('click', () => {
     inputMessage.value = '';
     const text = JSON.stringify({
         type: 'message',
-        name: 'sergey',
-        message: message
+        name: myInfo.name,
+        message: message,
+        chatID: myInfo.currentChatID,
+        ID: myInfo.ID
     });
+    myUtil.addMessage(myInfo.name, message, blockMessages)
     webSocketServer.send(text);
 })
--- src/util.js
+++ src/util.js
@@ -4,9 +4,21 @@ function addMessage(name, message, element) {
         <span id="name">${name}</span>: <span id="message">${message}</span>
     </div>`);
 }
+function userLeftMessage(currChatID, name, messageBlockElement) {
+    messageBlockElement.insertAdjacentHTML('beforeend', 
+    `<div class="message">
+        <span id="name">${name}</span> <span id="message">Left from the chat :(</span>
+    </div>`);
+}
+function addPesonInCurrentChat(user, chat, chatElement) {
+    chatElement.insertAdjacentHTML('beforeend', 
+    `<div class="user" data-id="${user.ID}">${user.name}</div>`);
+}
 
 const myUtil = {
-    addMessage
+    addMessage,
+    userLeftMessage,
+    addPesonInCurrentChat,
 }
 
 export default myUtil;
